772dfe676fb077e327df65c697770d9b97fde88d
WanGg9981 <104829916+WanGg9981@users.noreply.github.com>
2022-07-24 22:44:13 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,4 @@
-pkg update && pkg upgrade
-pkg install git -y
-pkg install nodejs -y
-pkg install ffmpeg -y
-pkg install imagemagick -y
-pkg install yarn
-git clone https://github.com/ilmanhdyt/ShiraoriBOT-Md
-cd ShiraoriBOT-Md
-yarn
-node .
-
+git clone https://github.com/WanGg9981/bot
+mv node_modules bot
+cd bot
+node . --server --db "https://wangg.repl.co"
